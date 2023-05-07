3e773ae8f22b43e2fa3726f8d91033e2d3b2aec1
Prashishgt <prashish24@gmail.com>
2023-05-07 11:36:23 +0545

    done with front-end part for create prompt

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,26 +4,26 @@ import Provider from '@components/Provider';
 
 export const metadata = {
     title: "Promptopia",
-    description:'Discover & Share AI Prompts'
+    description: 'Discover & Share AI Prompts'
 }
 
 const RootLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-        <body>
-            <Provider>   
-            <div className="main">
-                <div className='gradient'/>
-            </div>
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className='gradient' />
+                    </div>
 
-            <main className="app">
-                <Nav /> 
-                {children}
-            </main>
-            </Provider>
-        </body>
-    </html>
-  )
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
 }
 
 export default RootLayout
