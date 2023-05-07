3384fc13a2bbc2f854d6e92cf43fac9d14d461c6
Prashishgt <prashish24@gmail.com>
2023-05-07 09:23:11 +0545

    added provider through google login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-
-
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
     providers: [
@@ -12,10 +12,38 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionuser = await User.findOne({
+            email: session.user.email
+        })
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
 
     },
     async signIn({ profile }){
+        try {
+            await connectToDB();
 
+            // check if a user already exist
+            const UserExists = await User.findOne({
+                email:profile.email
+            });
+
+            // if not create
+            if(!UserExists) {
+                await User.create({
+                    email:profile.email,
+                    username:profile.name.replace(" ", "").
+                    toLowerCase(),
+                    image: profile.picture
+                })
+            }
+            return true
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
     
 })
